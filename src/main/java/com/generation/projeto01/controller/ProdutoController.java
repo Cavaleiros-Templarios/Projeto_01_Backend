b80cc87c7487d5653f91c03c2503d89d3db461ef
Wesley Lima <contato.wezzlim@outlook.com>
2025-05-29 10:58:06 -0300

    :sparkles: Implementação do método Buscar por nome e Criação Produto

--- src/main/java/com/generation/projeto01/controller/ProdutoController.java
+++ src/main/java/com/generation/projeto01/controller/ProdutoController.java
@@ -8,11 +8,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.projeto01.model.Produto;
 import com.generation.projeto01.repository.ProdutoRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/produtos")
@@ -23,17 +27,29 @@ public class ProdutoController {
 	private ProdutoRepository produtoRepository;
 	
 	@GetMapping
-	public ResponseEntity<List<Produto>> getAll() {
+	public ResponseEntity<List<Produto>> getAll()
+	{
 		return ResponseEntity.ok(produtoRepository.findAll());
 	}
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<Produto> getById(@PathVariable Long id) {
+	public ResponseEntity<Produto> getById(@PathVariable Long id)
+	{
 		return produtoRepository.findById(id)
 				.map(resposta -> ResponseEntity.ok(resposta))
-				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-				
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());		
 	}
 	
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Produto>> getAllByNome(@PathVariable String nome)
+	{
+		return ResponseEntity.ok(produtoRepository.findAllByNomeContainingIgnoreCase(nome));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Produto> post(@Valid @RequestBody Produto produto)
+	{
+		return ResponseEntity.status(HttpStatus.CREATED).body(produtoRepository.save(produto));
+	}
 
 }
